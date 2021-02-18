d0c5b58b6a3714ff9085183ae769eb3878f1c97a
Kate Mills <kate.mills1124@gmail.com>
2021-02-18 09:49:15 -0800

    constant links

---
+++ src/constants/links.js
@@ -0,0 +1,19 @@
+const links = [
+  {
+    page: 'Home',
+    path: '/',
+    id: 1,
+  },
+  {
+    page: 'About Us',
+    path: '/about-us',
+    id: 2,
+  },
+  {
+    page: 'Contact Us',
+    path: '/contact-us/',
+    id: 3,
+  }
+];
+
+export default links;
