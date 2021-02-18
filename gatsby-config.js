edf6e2dc445184ab73cdf8e4d8fd7431640a01fd
Kate Mills <kate.mills1124@gmail.com>
2021-02-18 09:12:41 -0800

    seo component

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,12 @@
 module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
+    titleTemplate: `%s | Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    baseUrl: `https://gatsby-seo-business-starter.netlify.app`, // No trailing slash allowed!
+    image: `/images/gatsby-astronaut.png`, // Path to your image you placed in the 'static' folder
+    author: `kate-mills`,
+    twitterUsername: `@gatsbyjs`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
--- src/components/layout.js
+++ src/components/layout.js
@@ -9,6 +9,7 @@ import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
+import SEO from "./seo"
 import Header from "./header"
 import "./layout.css"
 
@@ -25,6 +26,7 @@ const Layout = ({ children }) => {
 
   return (
     <>
+      <SEO/>
       <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
       <div
         style={{
