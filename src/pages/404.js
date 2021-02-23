db3d922ca2b1cd157f9535c479d7e0c425f1d491
Kate Mills <kate.mills1124@gmail.com>
2021-02-23 12:16:54 -0800

    added link back to home

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,5 +1,6 @@
 import React from "react"
 
+import {Link} from "gatsby"
 import Layout from "../components/layout"
 import { SEO } from "../components/Complete"
 
@@ -8,6 +9,7 @@ const NotFoundPage = () => (
     <SEO title="Oops!" />
     <h1>Oops!</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Link to="/">Back Home</Link>
   </Layout>
 )
 
