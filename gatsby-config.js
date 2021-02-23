45e9f596726509bf8e6c96e438cda2ab185688ad
Kate Mills <kate.mills1124@gmail.com>
2021-02-23 13:04:11 -0800

    Monserrat font

--- gatsby-config.js
+++ gatsby-config.js
@@ -75,7 +75,6 @@ module.exports = {
     `gatsby-plugin-sitemap`,
     `gatsby-plugin-netlify`,
     `gatsby-plugin-styled-components`,
-
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
